c7544cc69c60abcd81a852f760fef646b45ed94e
Grigory Griko <griko1996@gmail.com>
2023-07-17 22:29:00 +0300

    feat: типизация react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,7 @@
-import axios from 'axios';
-import React, { useEffect, useState } from 'react';
 import Card, { CardVariant } from './components/Card';
-import List from './components/List';
-import TodoItem from './components/TodoItem';
-import UserItem from './components/UserItem';
 import EventsExample from './components/EventsExample'
-import { ITodo, IUser } from './types/types';
-import {BrowserRouter, Route} from 'react-router-dom';
 
 const App = () => {
-  const [users, setUsers] = useState<IUser[]>([])
-  const [todos, setTodos] = useState<ITodo[]>([])
-
   return (
     <div>
       <EventsExample/>
