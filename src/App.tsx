64484a5cddab93ee7ec0bdb23f9b9e0bd1557e0f
Grigory Griko <griko1996@gmail.com>
2023-07-25 21:37:16 +0300

    feat: Добавление роутинга

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Card, { CardVariant } from './components/Card';
 import EventsExample from './components/EventsExample'
+import TodosPage from './components/TodosPage';
+import UsersPage from './components/UsersPage';
 
 const App = () => {
   return (
-    <div>
-      <EventsExample/>
-
-      <Card
-      onClick={() => console.log('нажатие произошло')}
-        variant={CardVariant.outlined}
-        width='200px'
-        height='200px'
-      >
-        <button>Кнопка</button>
-      </Card>
-
-    </div>
+    <BrowserRouter>
+      <div>
+        <Routes>
+          <Route path={'/users'} element={<UsersPage/>}/>
+          <Route path={'/todos'} element={<TodosPage/>}/>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 };
 
